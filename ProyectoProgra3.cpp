fb20f2d4da845c976e7d7dc45e7996796812d9e7
MrAle456 <138752745+MrAle456@users.noreply.github.com>
2025-09-18 03:35:04 -0600

    Update ProyectoProgra3.cpp

--- ProyectoProgra3.cpp
+++ ProyectoProgra3.cpp
@@ -22,9 +22,7 @@ Agrega el elemento en la lista, se utiliza new
 
 */
 
-/*
-TODO: Agregar numero de parqueo
-*/
+
 
 void Agregar(Nodo*& cabeza, char plate[6], string model, int hour, int parking )
 {
@@ -46,34 +44,39 @@ Recorre todos los elementos de la lista.
 	Ejecuta sin parar el codigo
 	Supongo que es un problema con que no se mueve al siguiente nodo
  	-Denis
+
+    Si eso era XD
+    -Ale
 */
 
 void Mostrar(Nodo*& cabeza){
     Nodo* actual = cabeza;
+
+    if(actual==nullptr){
+            cout<<"La lista esta vacia."<<endl;
+    }
+
     while(actual!=nullptr){
         cout<<actual->num_parqueo<<" -> "<<actual->placa<<" -> "<<actual->modelo<<" -> "<<actual->hora_ingreso<<" \n";
         actual = actual->siguiente;
     }
-    cout<<"nulo X_X"<<endl;
+
+    
+    cout<<"No hay mas elementos..."<<endl;
+    
 }
 
-/*
-
-Aqui uno ingresa el numero de placa del vehiculo a buscar y se imprime
-TODO: realizar pruebas de que funciona
-para su funcion se tiene que ingresar el nodo y el numero de placa
-
-*/
 
 void MostrarElemento(Nodo*& cabeza, int parqueo){
     Nodo* seleccion = cabeza;
-    while(seleccion!=nullptr){
+    
         if(seleccion->num_parqueo==parqueo){
         cout<<seleccion->num_parqueo<<" -> "<<seleccion->placa<<" -> "<<seleccion->modelo<<" -> "<<seleccion->hora_ingreso<<" \n";
+        
         }else{
             cout<<"No se encuentra el vehiculo :(";
         }
-    }
+    
 }
 
 
@@ -92,6 +95,10 @@ void limpiar(Nodo*& cabeza){
         delete temp;
     }
     cabeza=nullptr;
+
+    if(cabeza==nullptr){
+        cout<<"La memoria ha sido liberada con exito :D"<<endl;
+    }
 }
 
 /*
@@ -103,33 +110,20 @@ Salida de los vehiculo
 void Salida(Nodo*& cabeza, int parqueo){
 
     Nodo* actual=cabeza;
-    while(actual != nullptr){
 
+        while(actual!=nullptr)
         if(actual->num_parqueo==parqueo){
             Nodo* temp = actual;
             delete temp;
-        }
-    }
-    cabeza=nullptr;
-}
-
-bool comprobarRecorrido(Nodo*& cabeza){
-    Nodo* actual=cabeza;
-    Nodo* next=cabeza;
-
-    while (actual!=nullptr){
-        next=next->siguiente;
-    
-        if( (actual->hora_ingreso==next->hora_ingreso) && (actual->num_parqueo==next->num_parqueo)){
-            return false;
+            break;
         }else{
-            actual=actual->siguiente;
+            cout<<"No se encuentra en la lista."<<endl;
         }
-        return true;
-
-    }
-
+    
+    
 }
+
+
 
 /*
 
@@ -148,6 +142,7 @@ void menu(Nodo*& cabeza){
     int hora_ingreso;
     int parqueo;
 
+
     do {
         system("cls"); // Limpia la pantalla
 
@@ -156,10 +151,11 @@ void menu(Nodo*& cabeza){
         cout << "2. Agregar un vehiculo" << endl;
         cout << "3. Quitar un vehiculo del estacionamiento" << endl;
         cout << "4. Buscar un vehiculo" << endl;
-        cout << "5. Salir" << endl;
+        cout << "5. Manual de Uso" << endl;
+        cout << "6. Salir"<<endl;
         cout << "Escriba la Opcion que desea: ";
         cin >> opc;
-
+        
         switch (opc) {
             case 1:
                 Mostrar(cabeza);
@@ -180,7 +176,7 @@ void menu(Nodo*& cabeza){
 				
 				//este while asegura que el usuario no ingrese mas de 5 caracteres para la placa
 				while((cin.fail() && (strlen(placa) == 5 || strlen(placa)==0 ) ) ){
-                	cout<<"Ingreso mas de 5 caracteres. Intente otravez.";
+                	cout<<"Ingreso mas de 5 caracteres. Intente otravez."<<endl;
                 	cin.clear();
 	  				cin.ignore(10000, '\n');
                     cout << "Ingrese la placa: ";
@@ -194,17 +190,20 @@ void menu(Nodo*& cabeza){
                 cout << "Ingrese la hora de ingreso: ";
 
                 while(!(cin >> hora_ingreso) || (hora_ingreso<0 || hora_ingreso>23) ){ // Si hora_ingreso no es un int, corre el while. Solo se permitira ingresar hasta que sea un int hora_ingreso
-                	cout<<"Ingreso un valor que no es permitido. Intente otravez.";
+                	cout<<"Ingreso un valor que no es permitido. Intente otravez."<<endl;
                 	cin.clear();
 	  				cin.ignore(10000, '\n');
+                    cin>>hora_ingreso;
 				}            
 				
                 cout<<"Ingrese el numero de parqueo: ";
                 
-                while(!(cin >> hora_ingreso) || !comprobarRecorrido(cabeza)){
-                    cout<<"No se puede ingresar, el parqueo esta ocupado :( "<<endl;
+                while( !(cin >> parqueo) || (parqueo<1 || parqueo>999) ){
+                    cout<<"No se puede ingresar, valor fuera de rango :("<<endl;
                     cin.clear();
 	  				cin.ignore(10000, '\n');
+                    cout<<"Ingrese el numero de parqueo: ";
+                    cin>>parqueo;
                 }
 
                 Agregar(cabeza, placa, modelo, hora_ingreso, parqueo);
@@ -216,6 +215,7 @@ void menu(Nodo*& cabeza){
                 cout << "Ingrese el numero de parqueo del vehiculo a salir: ";
                 cin >> parqueo;
                 Salida(cabeza, parqueo);
+
                 cout << "Vehiculo retirado (si existe)." << endl;
                 system("pause");
                 break;
@@ -227,13 +227,34 @@ void menu(Nodo*& cabeza){
                 system("pause");
                 break;
 
-            case 5:
+            case 6:
                 limpiar(cabeza); // Llama a limpiar antes de salir
-                cout << "Saliendo del programa y liberando memoria." << endl;
+                cout << "Saliendo del programa" << endl;
                 cout<<"BYE OWO."<<endl;
                 exit(0);
                 break;
 
+            case 5:
+                system("cls");
+                cout<<"     MANUAL DE USO DEL PROGRAMA     \n"<<endl;
+                cout<<"Agregar: "<<endl;
+                cout<<"En el numero de placa se permiten caracteres numericos y letras pero solo se limitan a 5 caracteres. "<<endl;
+                cout<<"En la hora de ingreso solo se permiten de 0 a 23 horas."<<endl;
+                cout<<"En el numero de parque solo se admiten desde el 1 al 999"<<endl;
+                cout<<"\n ///////////////////////////////////////////////////////// \n\n";
+
+                cout<<"Mostrar elemento: "<<endl;
+                cout<<"Ingrese el numero del parqueo y se mostrara el vehiculo que busca."<<endl;
+                cout<<"\n ///////////////////////////////////////////////////////// \n\n";
+                
+                cout<<"Quitar un vehiculo del estacionamiento: "<<endl;
+                cout<<"Ingrese el numero del parqueo y se eliminara el vehiculo de la lista."<<endl;
+
+                 cout<<"\n ///////////////////////////////////////////////////////// \n\n";
+               
+                 cout<<"Si lee esto, le doy las gracias por darme la oportunidad de aprender :) \n -Grupo 4"<<endl;
+                system("pause");
+                break;
             default:
                 cout << "Opcion seleccionada invalida, por favor intente de nuevo." << endl;
                 system("pause");
@@ -241,16 +262,11 @@ void menu(Nodo*& cabeza){
         }
         system("cls");
 
-    } while (opc != 5);
+    } while (opc != 6);
 };
 
 
-/* Bug extrano:
-	Dice a la hora de ejecucion: 
-		"clear" no se reconoce como un comando interno o externo,
-		programa o archivo por lotes ejecutable.
 
-*/
 int main(){
     Nodo* cabeza = nullptr; // Declara e inicializa la cabeza de la lista
     menu(cabeza); // Pasa la cabeza al menú
